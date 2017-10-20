65d2656837d114a9d3b5cf929b11933c7ce03d64
Glitch (ncht) <none>
2017-10-20 02:41:00 +0000

    from glitch

---
+++ app.js
@@ -0,0 +1,42 @@
+var express = require('express'),
+    app = express(),
+    server = require('http').createServer(app),
+    io = require("socket.io").listen(server),
+    nicknames = {};
+
+//server.listen(8000);
+server.listen(process.env.PORT, process.env.IP);
+
+
+app.get('/', function(req, res) {
+    res.sendfile(__dirname + '/index.html');
+});
+
+
+io.sockets.on('connection', function(socket) {
+  
+    socket.on('send message', function(data) {
+        io.sockets.emit('new message', {msg: data, nick: socket.nickname});
+    });
+    
+    socket.on('new user', function(data, callback) {
+        if (data in nicknames) {
+            callback(false);
+        } else {
+            callback(true);
+            socket.nickname = data;
+            nicknames[socket.nickname] = 1;
+            updateNickNames();
+        }
+    });
+    
+    socket.on('disconnect', function(data) {
+        if(!socket.nickname) return;
+        delete nicknames[socket.nickname];
+        updateNickNames();
+    });
+    
+    function updateNickNames() {
+        io.sockets.emit('usernames', nicknames);
+    }
+});
